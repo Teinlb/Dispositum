9eddffbb169e5228fd458b55c53580f9df38a892
teinlb <tein.lebouille@gmail.com>
2024-09-04 23:28:01 +0200

    modified:   src/type.ts

--- src/type.ts
+++ src/type.ts
@@ -1,7 +1,13 @@
 type TasksType = {
     main: string[];
-    sub: { [key: string]: string[] }; // sub is altijd aanwezig en getypeerd
-    [key: string]: string[];
+    sub: { [key: string]: string[] }; // sub is een object met specifieke string array properties
+    Monday: string[];
+    Tuesday: string[];
+    Wednesday: string[];
+    Thursday: string[];
+    Friday: string[];
+    Saturday: string[];
+    Sunday: string[];
 };
 
 export default TasksType;
