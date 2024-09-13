adc1fa71d1bc0018f383f01326527ab5417b1072
teinlb <tein.lebouille@gmail.com>
2024-09-13 20:33:30 +0200

    jaJA

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
     plugins: [react()],
     base: "https://teinlb.github.io/Dispositum/",
-    build: {
-        outDir: "dist", // Zorg ervoor dat de output naar de `dist` map gaat
-    },
 });
